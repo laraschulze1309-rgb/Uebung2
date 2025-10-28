0b7acbf801f0d9594c4c689fff87f464fa696d5d
Lara Schulze <laraschulze@MacBook-Air-von-Lara.local>
2025-10-28 10:21:57 +0100

    p2

---
+++ p2/P2_main.java
@@ -0,0 +1,14 @@
+package p2;
+
+public class P2_main {
+    public static void main(String[] args) {
+        int i = 15;
+        boolean b = false;
+
+        else if (i > 10) {
+            b = true;
+        }
+
+        System.out.println(b);
+        }
+}
